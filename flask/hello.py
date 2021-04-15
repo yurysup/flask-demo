66c5ebc617b41d0eb05d467776ae6cedc60e7d8f
Yury Suponeu <usuponev@gmail.com>
2021-04-15 23:21:27 +0300

    Flask: Swagger added

--- flask/hello.py
+++ flask/hello.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from flasgger import Swagger
+
 app = Flask(__name__)
+Swagger(app)
 
 @app.route('/')
 def hello_world():
-    print('hi!')
+    """
+    A simple test API
+    This ednpoint does nothing
+    Only returs "Hello, World!"
+    ---
+    responses:
+      200:
+        description: Hello, World! message
+    """
     return 'Hello, World!'
